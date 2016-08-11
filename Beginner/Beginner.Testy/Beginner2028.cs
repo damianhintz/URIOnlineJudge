76cde29c971f4dfe41e38d2a1fd12b79af1de926
Damian Hintz <damian.hintz@gmail.com>
2016-08-11 14:13:57 +0200

    Dodaj nowy problem 2060

---
+++ Beginner/Beginner.Testy/Beginner2028.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Beginner.Testy
+{
+    [TestClass]
+    public class Beginner2028
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Beginner/Beginner.Testy/Beginner2060.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Beginner.Testy
+{
+    [TestClass]
+    public class Beginner2060
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
