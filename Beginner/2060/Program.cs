4ed7e8d0f61501916252b7a31f5e10780ac6d161
Damian Hintz <damian.hintz@gmail.com>
2016-08-11 14:36:02 +0200

    Solution of the problem 2060

--- Beginner/2060/Program.cs
+++ Beginner/2060/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Beginner2060
 {
@@ -10,6 +7,19 @@ namespace Beginner2060
     {
         static void Main(string[] args)
         {
+            var line = Console.ReadLine().Trim();
+            var amountOfNumbers = int.Parse(line);
+            var numbersLine = Console.ReadLine().Trim();
+            var numbersSplit = numbersLine.Split(' ').Take(amountOfNumbers);
+            var numbers = from n in numbersSplit select int.Parse(n);
+            var multiplesOf2 = numbers.Where(n => n % 2 == 0);
+            Console.WriteLine("{0} Multiplo(s) de 2", multiplesOf2.Count());
+            var multiplesOf3 = numbers.Where(n => n % 3 == 0);
+            Console.WriteLine("{0} Multiplo(s) de 3", multiplesOf3.Count());
+            var multiplesOf4 = numbers.Where(n => n % 4 == 0);
+            Console.WriteLine("{0} Multiplo(s) de 4", multiplesOf4.Count());
+            var multiplesOf5 = numbers.Where(n => n % 5 == 0);
+            Console.WriteLine("{0} Multiplo(s) de 5", multiplesOf5.Count());
         }
     }
 }
