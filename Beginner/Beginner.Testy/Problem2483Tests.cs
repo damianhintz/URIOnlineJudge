d6f2eadc7d16fa6f1983c74792349e6c97b8decd
Damian Hintz <damian.hintz@gmail.com>
2018-08-20 22:37:09 +0200

    Dodaj rozwiązanie problemu 2483

---
+++ Beginner/Beginner.Testy/Problem2483Tests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Problem2483;
+
+namespace Beginner.Testy
+{
+    [TestFixture]
+    public class Problem2483Tests
+    {
+        [Test]
+        public void Given_HappinessIndexIsOne_When_WeWantToKnowMerryChristmasInPortuguese_Then_ShouldBeFelizNatal()
+        {
+            var solver = new ProblemSolver2483();
+
+            var answer = solver.GetMerryChristmasInPortuguese(1);
+
+            answer.Should().Be("Feliz natal!");
+        }
+
+        [Test]
+        public void Given_HappinessIndexIsTwo_When_WeWantToKnowMerryChristmasInPortuguese_Then_ShouldBeFelizNataal()
+        {
+            var solver = new ProblemSolver2483();
+
+            var answer = solver.GetMerryChristmasInPortuguese(2);
+
+            answer.Should().Be("Feliz nataal!");
+        }
+    }
+}
---
+++ Beginner/Problem2483/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Problem2483
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var happinessIndex = int.Parse(Console.ReadLine());
+            var answer = new ProblemSolver2483()
+                .GetMerryChristmasInPortuguese(happinessIndex);
+            Console.WriteLine(answer);
+        }
+    }
+
+    public class ProblemSolver2483
+    {
+        public string GetMerryChristmasInPortuguese(int happinessIndex)
+        {
+            var repeatedA = new string('a', happinessIndex);
+            return $"Feliz nat{repeatedA}l!";
+        }
+    }
+}
