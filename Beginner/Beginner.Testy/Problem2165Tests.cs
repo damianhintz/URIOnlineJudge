333ad7478d9af1e952d79f71aa06184899b00561
Damian Hintz <damian.hintz@gmail.com>
2018-09-09 17:23:20 +0200

    Dodaj rozwiązanie problemu 2165

---
+++ Beginner/Beginner.Testy/Problem2165Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using Problem2165;
+
+namespace Beginner.Testy
+{
+    [TestFixture]
+    public class Problem2165Tests
+    {
+        [Test]
+        public void Text140InLengthIsTweet()
+        {
+            var solver = new ProblemSolver2165();
+            var text141Length = new string('t', 140);
+            var answer = solver.IsTweet(text141Length);
+            answer.Should().BeTrue();
+        }
+
+        [Test]
+        public void TextLongerThan140IsNotTweet()
+        {
+            var solver = new ProblemSolver2165();
+            var text141Length = new string('t', 141);
+            var answer = solver.IsTweet(text141Length);
+            answer.Should().BeFalse();
+        }
+    }
+}
---
+++ Beginner/Problem2165/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Problem2165
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+            var isTweet = new ProblemSolver2165().IsTweet(text);
+            var answer = isTweet ? "TWEET" : "MUTE";
+            Console.WriteLine(answer);
+        }
+    }
+
+    public class ProblemSolver2165
+    {
+        private const int TWEET_MAX_LENGTH = 140;
+
+        public bool IsTweet(string text)
+        {
+            return text.Length <= TWEET_MAX_LENGTH;
+        }
+    }
+}
